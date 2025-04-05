1e08f8181e64be41b232e3d5e94408d468b1d33e
정민호 <allmin9702@naver.com>
2025-04-06 00:08:11 +0900

    [BE] Init: Project initialize

---
+++ EverSubServer/src/main/java/com/bjduo/eversubserver/EverSubServerApplication.java
@@ -0,0 +1,13 @@
+package com.bjduo.eversubserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EverSubServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EverSubServerApplication.class, args);
+    }
+
+}
---
+++ EverSubServer/src/test/java/com/bjduo/eversubserver/EverSubServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bjduo.eversubserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EverSubServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
